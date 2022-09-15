bcdf7f2f3aa0296f2728bb29ae2800aeb358ba4e
vicky42119 <91129632+vicky42119@users.noreply.github.com>
2022-09-14 21:53:48 -0400

    update

--- style.js
+++ style.js
@@ -1,17 +1,25 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+var slideIndex = [1,1,1,1,1];
+var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+showSlides(1, 4);
+showSlides(1, 5);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
-
-function showDivs(n) {
+function currentSlide(n, no) {
+  showSlides(slideIndex[no] = n, no);
+}
+function showSlides(n, no) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = x.length} ;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
+     x[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block"; 
+  x[slideIndex[no]-1].style.display = "block";  
 }
